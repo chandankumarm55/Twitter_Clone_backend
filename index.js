e8fc2628d1d6c8551368e9e7130a048fcac50f87
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2024-06-12 08:27:54 +0530

    ready for deploy

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: process.env.FRONT_END_URL,
     credentials: true
 }
 app.use(cors(corsOptions))
