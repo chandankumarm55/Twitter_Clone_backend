70285f2f004ca4bf064219a57d7303308cc3bf03
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2024-06-12 16:06:13 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,23 +1,29 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-dotenv.config({
-    path:"../config/.env"
-})
+dotenv.config()
 
-const isAuthenticated = async (req,res,next) => {
+
+const isAuthenticated = async (req, res, next) => {
     try {
-        const token = req.cookies.token; 
-        if(!token){
+        const token = req.cookies.token;
+        if (!token) {
             return res.status(401).json({
-                message:"User not authenticated.",
-                success:false
-            })
+                message: "User not authenticated.",
+                success: false
+            });
         }
-        const decode = await jwt.verify(token, process.env.TOKEN_SECRET);
-        req.user = decode.userId;
+console.log(token)
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+        req.user = decoded.userId;
+        console.log(req.user)
         next();
     } catch (error) {
         console.log(error);
+        return res.status(401).json({
+            message: "User not authenticated.",
+            success: false
+        });
     }
 }
+
 export default isAuthenticated;
